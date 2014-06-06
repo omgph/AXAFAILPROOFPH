e579e25de6a17c28da0f14993cfb9fcb1e9049fc
omgph <shay.takeuchi@omnicommediagroup.com>
2014-06-06 11:04:35 +0800

    Views and Images
    Views and images upload

---
+++ AxaFailProof/Global.asax.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AxaFailProof
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "FailProofYourHealth", // Route name
+                "Fail-Proof-Your-Health", // URL with parameters
+                new { controller = "Home", action = "FailProofYourHealth", id = 7 } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "HealthRiskScore", // Route name
+                "Health-Risk-Score", // URL with parameters
+                new { controller = "Home", action = "HealthRiskScore"} // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "HealthFactsFigures", // Route name
+                "Health-Facts-Figures", // URL with parameters
+                new { controller = "Home", action = "HealthFactsFigures", id = 11 } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "HealthFactsFiguresDetails", // Route name
+                "Health-Facts-Figures/{id}/{title}", // URL with parameters
+                new { controller = "Home", action = "HealthDetail", id = UrlParameter.Optional, title = UrlParameter.Optional } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "HealthMax", // Route name
+                "Health-Max", // URL with parameters
+                new { controller = "Home", action = "FailProofYourHealth", id = 9 } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "HealthExentials", // Route name
+                "Health-Exentials", // URL with parameters
+                new { controller = "Home", action = "FailProofYourHealth", id = 8 } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "WhatIsFailProof", // Route name
+                "what-is-fail-proof", // URL with parameters
+                new { controller = "Home", action = "WhatIsFailProof", id = 3 } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "StoryDetails", // Route name
+                "Story/{id}/{title}", // URL with parameters
+                new { controller = "Home", action = "Story", id = UrlParameter.Optional, title = UrlParameter.Optional } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "SEO", // Route name
+                "{seo}/{action}", // URL with parameters
+                new { controller = "Home", action = "Index", seo = UrlParameter.Optional} // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "ArchiveByDate", // Route name
+                "{action}/{month}/{year}", // URL with parameters
+                new { controller = "Home", action = "ArchiveByDate", month = UrlParameter.Optional, year = UrlParameter.Optional } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "NotFound", // Route name
+                "NotFound", // URL with parameters
+                new { controller = "Home", action = "NotFound" } // Parameter defaults
+            );
+
+
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ AxaFailProof/ViewModel/AxaViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AxaFailProof.Models;
+using System.Collections;
+using PagedList;
+using Utils;
+
+namespace AxaFailProof.Models
+{
+    public class AxaViewModel
+    {
+        public List<Banner> HomeBanners { get; set; }
+        public IEnumerable<Banner> MainBanner { get; set; }
+        public IEnumerable<Banner> Banner { get; set; }
+        public IEnumerable<Banner> FotterBanner { get; set; }
+        public IEnumerable<Story> FeaturedStory { get; set; }
+        public IEnumerable<Story> ResentPost { get; set; }
+        public IEnumerable<Story> StoryDetail { get; set; }
+        public IEnumerable<Story> HealthFacts { get; set; }
+
+        public List<Location> Locations { get; set; }
+        public List<Story> Stories { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Topic> Topics { get; set; }
+        public List<Story> DateArchive { get; set; }
+
+        public IPagedList<Story> SearchResults { get; set; }
+
+        public Mushroom Mushroom1 { get; set; }
+        public Mushroom Mushroom2 { get; set; }
+        public Mushroom Mushroom3 { get; set; }
+        public Mushroom Mushroom4 { get; set; }
+
+        public Mushroom Question1 { get; set; }
+        public Mushroom Question2 { get; set; }
+        public Mushroom Question3 { get; set; }
+        public Mushroom Question4 { get; set; }
+        public Mushroom Question5 { get; set; }
+        public Mushroom Question6 { get; set; }
+
+        public StaticPage StaticPage { get; set; }
+        public EventsRegistered EventsRegistered { get; set; }
+        public ContactU ContactUs { get; set; }
+        public Story Story { get; set; }
+        public Comment Comment { get; set; }
+        public Quiz Quiz { get; set; }
+        public SocialUser SocialUser { get; set; }
+        public FailProofing FailProofing { get; set; }
+        public YahooProfile Profile { get; set; }
+
+        public int StoryID { get; set; }
+        public string SearchBox { get; set; }
+    }
+}
